3bdd2a65080fbd2d054ac07744b29857c61044d0
Daniel Mora Gonzalez <danielmglz@outlook.com>
2022-01-16 12:48:54 +0100

    modified
    modified

---
+++ main.js
@@ -0,0 +1,13 @@
+const projectName = 'javascript-calculator';
+
+const isOperator = /[x/+‑]/,
+  endsWithOperator = /[x+‑/]$/,
+  endsWithNegativeSign = /\d[x/+‑]{1}‑$/,
+  clearStyle = { background: '#ac3939' },
+  operatorStyle = { background: '#666666' },
+  equalsStyle = {
+    background: '#004466',
+    position: 'absolute',
+    height: 130,
+    bottom: 5
+  };
